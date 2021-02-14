bb85ca672de69903bf473564dd43a7412bc3f03d
Sergius <72300097+Sergius-1488@users.noreply.github.com>
2021-02-14 14:53:09 +0200

    Update test_button_for_busket.py

--- test_button_for_busket.py
+++ test_button_for_busket.py
@@ -5,7 +5,7 @@ link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
 def test_book_shop(browser):
     browser.get(link)
     try:
-        browser.find_element_by_class_name('btn-add-to-basketы').tag_name
+        browser.find_element_by_class_name('btn-add-to-basket').tag_name
         result = True
     except NoSuchElementException:
         result = False
