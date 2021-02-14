112ea9aba397b1f784a32eed29e648eba5edc611
Sergius <72300097+Sergius-1488@users.noreply.github.com>
2021-02-14 14:52:13 +0200

    Update test_button_for_busket.py

--- test_button_for_busket.py
+++ test_button_for_busket.py
@@ -1,6 +1,14 @@
+from selenium.common.exceptions import NoSuchElementException
+
 link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
 
 def test_book_shop(browser):
     browser.get(link)
-    but=browser.find_element_by_class_name('btn-add-to-basket').tag_name
-    assert str(but) == "button"
+    try:
+        browser.find_element_by_class_name('btn-add-to-basketы').tag_name
+        result = True
+    except NoSuchElementException:
+        result = False
+    assert result == True, "Element not found"
+    
+ 
